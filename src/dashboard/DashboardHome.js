6b6723461a05ca19f4b68ef98da66e8e05dc2a7b
Apoorv Goel <apoorv.goel@indiamart.com>
2021-06-02 20:54:27 +0530

    some changes

--- src/dashboard/DashboardHome.js
+++ src/dashboard/DashboardHome.js
@@ -38,19 +38,31 @@ export default function DashboardHome(props) {
             });
     }
 
+    function viewHandle(id) {
+        // document.getElementById(id).style.display = "none";
+        let y = { FORM_ID: id }
+        console.log(JSON.stringify(y));
+        axios.post('http://survey3171.000webhostapp.com/api/showForm.php', JSON.stringify(y))
+            .then((response) => {
+                console.log(response);
+            }, (error) => {
+                console.log(error);
+            });
+    }
+
     var k;
     console.log(dataH[0]);
     if (dataH.length != 0)
-        k = dataH.map((ele, index) => 
+        k = dataH.map((ele, index) =>
             <div id={ele.FORM_ID} className="AK">
                 <h3 >{ele.FORM_NAME}</h3>
                 <a className="Share" href="whatsapp://send?text=This is WhatsApp sharing example using link" data-action="share/whatsapp/share" target="_blank">Share<i class="fa fa-share-alt" aria-hidden="true"></i></a>
                 {/* <i className="fas fa-trash-alt">Ankit</i> */}
                 <div className="buttons">
-                <a className="viewForm" onClick={() => deleteHandle(ele.FORM_ID)}>View Form</a>
-                <a className="editForm" onClick={() => deleteHandle(ele.FORM_ID)}>Edit</a>
-                <a className="deleteForm" onClick={() => deleteHandle(ele.FORM_ID)}>Delete</a>
-                <a className="showResponses" onClick={() => deleteHandle(ele.FORM_ID)}>Show Responses</a>
+                    <a className="viewForm" onClick={() => viewHandle(ele.FORM_ID)}>View Form</a>
+                    <a className="editForm" onClick={() => deleteHandle(ele.FORM_ID)}>Edit</a>
+                    <a className="deleteForm" onClick={() => deleteHandle(ele.FORM_ID)}>Delete</a>
+                    <a className="showResponses" onClick={() => deleteHandle(ele.FORM_ID)}>Show Responses</a>
                 </div>
                 {/* <i className="fa fa-search">Search</i> */}
                 {/* <DeleteIcon size='small' >Click Me</DeleteIcon> */}
@@ -63,8 +75,8 @@ export default function DashboardHome(props) {
                 <NewForm />
             </div>
             <div className="formsDiv">
-            <h1 className="formsHeading">Your Forms</h1>
-            <div className="showForms">{k}</div>
+                <h1 className="formsHeading">Your Forms</h1>
+                <div className="showForms">{k}</div>
             </div>
         </div>
     )
